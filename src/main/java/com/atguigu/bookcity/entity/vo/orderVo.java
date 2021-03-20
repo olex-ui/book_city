1784adc98d23a49c715171afabac00c9fbcfdf8c
olex-ui <2941176308@qq.com>
2021-03-20 13:37:06 +0800

    添加controller接口

---
+++ src/main/java/com/atguigu/bookcity/entity/vo/orderVo.java
@@ -0,0 +1,14 @@
+package com.atguigu.bookcity.entity.vo;
+
+import lombok.Data;
+
+/**
+ * @author hxx
+ * @date 2021/3/20 12:30
+ */
+@Data
+public class orderVo {
+    private String bookId;
+
+    private Integer bookNumber;
+}
