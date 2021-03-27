9d58c2cbd61578e8d6a37cdd34fa60362bd70df8
olex-ui <2941176308@qq.com>
2021-03-27 10:34:13 +0800

    添加业务实现

---
+++ src/main/java/com/atguigu/bookcity/service/OrderItemService.java
@@ -0,0 +1,13 @@
+package com.atguigu.bookcity.service;
+
+import com.atguigu.bookcity.entity.Category;
+import com.atguigu.bookcity.entity.orderItem;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author hxx
+ * @date 2021/3/26 23:03
+ */
+public interface OrderItemService extends IService<orderItem> {
+}
